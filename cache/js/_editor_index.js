2c71dd48806999fad2166b885d8c4028851afd99
Nilleren1 <nicolai.hoegsbjerg@gmail.com>
2024-03-03 13:13:41 +0100

    new meshes

--- cache/js/_editor_index.js
+++ cache/js/_editor_index.js
@@ -1,6 +1,6 @@
 import * as __wonderlandengine_components from '@wonderlandengine/components';
 _registerEditor(__wonderlandengine_components);
-import * as _C__Users_malth_Documents_WonderlandEngine_my_wonderland_js_button_js from 'C:/Users/malth/Documents/WonderlandEngine/my-wonderland/js/button.js';
-_registerEditor(_C__Users_malth_Documents_WonderlandEngine_my_wonderland_js_button_js);
-import * as _C__Users_malth_Documents_WonderlandEngine_my_wonderland_js_countdown_js from 'C:/Users/malth/Documents/WonderlandEngine/my-wonderland/js/countdown.js';
-_registerEditor(_C__Users_malth_Documents_WonderlandEngine_my_wonderland_js_countdown_js);
+import * as _C__Users_nicol_source_repos_php_my_wonderland_js_button_js from 'C:/Users/nicol/source/repos/php/my-wonderland/js/button.js';
+_registerEditor(_C__Users_nicol_source_repos_php_my_wonderland_js_button_js);
+import * as _C__Users_nicol_source_repos_php_my_wonderland_js_countdown_js from 'C:/Users/nicol/source/repos/php/my-wonderland/js/countdown.js';
+_registerEditor(_C__Users_nicol_source_repos_php_my_wonderland_js_countdown_js);
